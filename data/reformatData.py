50966ff59bbad436c7c41bda2b28da53e5c0efc6
Jessica Wang <jessica.wang27@trinityschoolnyc.org>
2025-05-06 14:56:17 -0400

    Finish reformatData.py

--- data/reformatData.py
+++ data/reformatData.py
@@ -4,23 +4,29 @@ import json
 f1 = open("life_expectancy_cleansed.csv", "r")
 lines = f1.readlines()
 
-dictionary ={
+dictionary ={}
 
-    "countries":{
-        "mexico":[],
-        "canada":[],
-        "us":[]
-    },
-    "years":[]
-
-}
-
-# Create the dictionary here
+# print(lines)
+nested_dict = {}
+for i in range(len(lines)):
+    line = lines[i].replace("\n","").replace("\ufeff","").split(",")
+    
+    if line[0] == 'Country Name':
+        dictionary['years'] = line[1:]
+    
+    else:
+        nested_dict[line[0]] = line[1:]
+        dictionary['countries'] = nested_dict
 
 f1.close()
+
 
 #Save the json object to a file
 f2 = open("life_expectancy.json", "w")
 json.dump(dictionary, f2, indent = 4)
 
+
 f2.close()
+
+
+
